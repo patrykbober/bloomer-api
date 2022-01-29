6b896f304698012add02326fdff239542b09552d
Patryk Bober <boberp26@gmail.com>
2022-01-29 22:31:23 +0100

    initial commit

---
+++ src/main/java/pl/patrykbober/bloomer/BloomerApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykbober.bloomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomerApplication.class, args);
+    }
+
+}
