275dc92a81358957afd2f4a4ab0ca6120df5f2ff
Patryk Bober <boberp26@gmail.com>
2022-03-24 00:36:00 +0100

    configure database, liquibase & repository fix

--- src/main/java/pl/patrykbober/bloomer/repository/BloomerUserRepository.java
+++ src/main/java/pl/patrykbober/bloomer/repository/BloomerUserRepository.java
@@ -1,5 +1,6 @@
 package pl.patrykbober.bloomer.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.patrykbober.bloomer.model.jpa.BloomerUser;
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface BloomerUserRepository extends JpaRepository<BloomerUser, Long> {
 
+    @EntityGraph(attributePaths = {"roles"})
     Optional<BloomerUser> findByEmail(String email);
 
 }
